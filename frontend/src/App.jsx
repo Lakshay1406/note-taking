5eaa458f11f398f6459f36a512af5cf671ce12ab
Lakshay1406 <142895484+Lakshay1406@users.noreply.github.com>
2024-12-13 17:14:57 +0530

    scroll change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,12 +15,13 @@ function App() {
 
   return (
     <>
-      <div className="bg-[url('/bookbg2.jpg')] bg-cover bg-center overflow-auto h-screen">
-        <div className="bg-gradient-to-r from-black/80 from-10% via-transparent to-transparent flex flex-col h-screen">
+
+      <div className="bg-[url('/bookbg2.jpg')] bg-cover bg-center overflow-auto h-screen scrollbar-dark">
+        <div className="bg-gradient-to-r from-black/80 from-10% via-transparent to-transparent flex flex-col h-full">
           <NavBar />
           <div className="p-5 sm:p-16 md:p-20 lg:p-28 flex items-start justify-center flex-col h-full">
             <h1 className="text-white font-bold text-5xl md:text-6xl lg:text-7xl">Notes That Stay with You,<br /> Always.</h1>
-            <h1 className="text-white text-2xl font-thin mt-6">Create, update, and delete your notes with ease,<br /> keeping them with you wherever you go.</h1>
+            <h1 className="text-white text-2xl  font-thin mt-6">Create, update, and delete your notes with ease,<br /> keeping them with you wherever you go.</h1>
             <Link to={loggedIn ? '/notes' : '/login'} className="mt-6 font-mono font-extrabold border border-yellow-500 rounded-md p-2 hover:bg-yellow-500 text-yellow-500 hover:text-gray-800 active:bg-gray-800 active:text-white">
               Get Started
             </Link>
